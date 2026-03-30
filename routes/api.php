cb075025432a98a6fc6b2b08d641545f5345f9ec
smt197 <serignembayet@gmail.com>
2026-03-30 10:29:49 +0000

    update ollama to qwen2.5:7b

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\OrchestratorController;
+
+use Orion\Facades\Orion;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/orchestrate', [OrchestratorController::class, 'generateFromAngular']);
+
